4ea2c642e269232d79fb92f43925885ed4ae5aa1
Maxwell Gove <mg3854@nyu.edu>
2016-09-24 08:19:24 +0000

    we actually don't need these

--- lib/active_force/relation/query_methods.rb
+++ lib/active_force/relation/query_methods.rb
@@ -2,17 +2,8 @@ module ActiveForce
   module Relation
     module QueryMethods
       
-      include ActiveForce::Relation::SOQL
-      
       def where(conditions)
         find_by_soql(build_query(conditions: conditions))
-      end
-      
-      
-      private
-      
-      def fields_for_soql
-        self.description['fields'].map { |f| f['name'] }.join(', ')
       end
       
     end
