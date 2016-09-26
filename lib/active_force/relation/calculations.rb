8b13993f888aea26d741c0cfcb896c9d3640dbab
Maxwell Gove <mg3854@nyu.edu>
2016-09-26 06:29:48 +0000

    simple calculations are now working

--- lib/active_force/relation/calculations.rb
+++ lib/active_force/relation/calculations.rb
@@ -2,9 +2,42 @@ module ActiveForce
   module Relation
     module Calculations
       
-      def count(conditions: nil)
-        q = build_query(fields: "COUNT()", conditions: conditions)
-        client.execute_soql(q)['totalSize']
+      def count(column_name = nil)
+        calculate(:count, column_name)
+      end
+      
+      def average(column_name)
+        calculate(:avg, column_name)
+      end
+      
+      def minimum(column_name)
+        calculate(:min, column_name)
+      end
+      
+      def maximum(column_name)
+        calculate(:min, column_name)
+      end
+      
+      def sum(column_name = nil, &block)
+        calculate(:sum, column_name)
+      end
+      
+      def pluck(*column_names)
+        
+      end
+      
+      def ids
+        pluck(:id)
+      end
+      
+      def calculate(operation, column_name)
+        selector = "#{operation}(#{column_name})"
+        
+        q = ActiveForce::Query.new(field_list: selector, object_type: self.sobject_name)
+        
+        result = Client.connection.execute_soql(q.to_soql)
+        
+        operation == :count ? result['totalSize'] : result['records'].first['expr0']
       end
       
     end
