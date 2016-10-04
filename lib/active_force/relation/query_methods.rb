787a074a5cd9a082640ab23f4400543213f352dc
Maxwell Gove <mg3854@nyu.edu>
2016-10-04 23:19:18 +0000

    or statements

--- lib/active_force/relation/query_methods.rb
+++ lib/active_force/relation/query_methods.rb
@@ -8,6 +8,21 @@ module ActiveForce
         querify.build(:where, sanitize_soql_for_assignment(conditions))
       end
       
+      def or(other)
+        if other.is_a?(ActiveForce::Query)
+          querify._or!(other)
+        else
+          raise "You have passed object of type #{other.class} to #or. Try passing an object of type ActiveForce::Query"
+        end
+      end
+      
+      def _or!(other)
+        check_valid_or_statement(other)
+        
+        self.condition_expression << " OR #{other.condition_expression}"
+        self
+      end
+      
       def select(*fields)
         querify._select!(*fields)
       end
@@ -37,6 +52,20 @@ module ActiveForce
         self.number_of_rows_to_skip = num_records
         self
       end
+      
+      private
+      
+        def check_valid_or_statement(other)
+          raise "Both statements must include a matching having or where clause to use or" if !_valid_or_statement?(other)
+        end
+        
+        def _valid_or_statement?(other)
+          # both have a where clause or a having clause
+    
+          [:condition_expression, :having_condition_expression].collect do |expr|
+            self.send(expr).present? && other.send(expr).present?
+          end.any?
+        end
       
     end
   end
