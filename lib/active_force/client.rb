edec79fa7da70ae792ddbd3d3cecbe309e901917
Maxwell Gove <mg3854@nyu.edu>
2016-10-08 21:16:54 +0000

    updates for client connection

--- lib/active_force/client.rb
+++ lib/active_force/client.rb
@@ -30,19 +30,19 @@ module ActiveForce
     end
       
     def get(id, klass)
-      endpoint_request(:get, "sobjects/#{klass.sobject_name}/#{id}")
+      rest_method(:get, id, klass)
     end
     
     def post(sobject)
-      
+      rest_method(:post, id, klass)
     end
     
     def patch(sobject)
-      
+      rest_method(:patch, id, klass)
     end
     
     def delete(sobject)
-      endpoint_request(:delete, "sobjects/#{sobject.class.sobject_name}/#{id}")
+      rest_method(:delete, id, klass)
     end
     
     def describe_all
@@ -81,54 +81,58 @@ module ActiveForce
     
     private
     
-    def authenticate
-      http = Net::HTTP.new(HOST, PORT)
-      http.use_ssl = true
-      
-      request = Net::HTTP::Post.new(AUTHENTICATION_ENDPOINT)
-      request.set_form_data({
-        :grant_type => 'password',
-        :client_id => CREDENTIALS[:consumer_key],
-        :client_secret => CREDENTIALS[:consumer_secret],
-        :username => CREDENTIALS[:username],
-        :password => "#{CREDENTIALS[:password]}#{CREDENTIALS[:security_token]}"
-      })
-      
-      response = http.request(request)
-      body_hsh = JSON.parse(response.body)
-      
-      @instance_host = body_hsh['instance_url'].gsub('https://','')
-      @access_token = body_hsh['access_token']
-    end
-    
-    def endpoint_uri(endpoint, params = {})
-      URI.encode("#{SERVICES_BASE}/#{VERSION}/#{endpoint}#{"/#{params.map { |k,v| "#{k.to_s}=#{v.to_s}"}}" if !params.empty?}")
-    end
-      
-    def endpoint_request(method, endpoint, data = {})
-      http = Net::HTTP.new(@instance_host, PORT)
-      http.use_ssl = true
-      
-      request = Net::HTTP.const_get(method.to_s.capitalize).new(endpoint_uri(endpoint, method == :get ? data : {}))
-      request['Authorization'] = "Bearer #{@access_token}"
-      request['Content-Type'] = 'application/json'
-      
-      unless data.empty? || method == :get
-        data.delete('Id') if data['Id']  # the id wil be specified in the request uri, not in the body
-        request.body = data.to_json
+      def authenticate
+        http = Net::HTTP.new(HOST, PORT)
+        http.use_ssl = true
+        
+        request = Net::HTTP::Post.new(AUTHENTICATION_ENDPOINT)
+        request.set_form_data({
+          :grant_type => 'password',
+          :client_id => CREDENTIALS[:consumer_key],
+          :client_secret => CREDENTIALS[:consumer_secret],
+          :username => CREDENTIALS[:username],
+          :password => "#{CREDENTIALS[:password]}#{CREDENTIALS[:security_token]}"
+        })
+        
+        response = http.request(request)
+        body_hsh = JSON.parse(response.body)
+        
+        @instance_host = body_hsh['instance_url'].gsub('https://','')
+        @access_token = body_hsh['access_token']
       end
       
-      response = http.request(request)
-      
-      raise "Salesforce API error #{response.code}: #{response.body} -- #{request.path} #{request.uri} #{request.body}" if response.is_bad?
-      
-      if response.body
-        JSON.parse(response.body)
-      else
-        {}
+      def endpoint_uri(endpoint, params = {})
+        URI.encode("#{SERVICES_BASE}/#{VERSION}/#{endpoint}#{"/#{params.map { |k,v| "#{k.to_s}=#{v.to_s}"}}" if !params.empty?}")
+      end
+        
+      def endpoint_request(method, endpoint, data = {})
+        http = Net::HTTP.new(@instance_host, PORT)
+        http.use_ssl = true
+        
+        request = Net::HTTP.const_get(method.to_s.capitalize).new(endpoint_uri(endpoint, method == :get ? data : {}))
+        request['Authorization'] = "Bearer #{@access_token}"
+        request['Content-Type'] = 'application/json'
+        
+        unless data.empty? || method == :get
+          data.delete('Id') if data['Id']  # the id wil be specified in the request uri, not in the body
+          request.body = data.to_json
+        end
+        
+        response = http.request(request)
+        
+        raise "Salesforce API error #{response.code}: #{response.body} -- #{request.path} #{request.uri} #{request.body}" if response.is_bad?
+        
+        if response.body
+          JSON.parse(response.body)
+        else
+          {}
+        end
+        
       end
       
-    end
+      def rest_method(method, id, klass)
+        endpoint_request(method, "sobjects/#{klass}/#{id}")
+      end
     
   end
 end
